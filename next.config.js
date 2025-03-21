d3f9484997b1d8e164f59ac9957f87baad566f90
Your Name <your-email@example.com>
2025-03-20 23:37:52 -0700

    Improve GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -3,10 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/kumi-math' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/kumi-math' : '',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig; 
